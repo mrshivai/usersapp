ebeb374e1d4f388afedb77dec04fff292a5c9ff8
jaydeepbendre <jaydeepd.bendre@gmail.com>
2023-08-04 18:25:26 +0530

    atlas

--- rest/db.js
+++ rest/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/usersapp_db", {
+mongoose.connect("mongodb+srv://jaydeep:cf7oXlVlFhxQmb9@cluster0.0z436rp.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4,
 });
 
 module.exports = mongoose.connection;
+
+// "mongodb+srv://jaydeep:cf7oXlVlFhxQmb9@cluster0.0z436rp.mongodb.net/?retryWrites=true&w=majority"
