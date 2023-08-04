e5e684efc625ca96bc2133300d3187f845115b1b
jaydeepbendre <jaydeepd.bendre@gmail.com>
2023-08-04 16:24:22 +0530

    gg2

--- rest/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/usersapp_db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    family: 4,
-});
-
-module.exports = mongoose.connection;
