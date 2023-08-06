9bae033269e81fd80e79c6f315dd91e982d8aad9
jaydeepbendre <jaydeepd.bendre@gmail.com>
2023-08-06 16:26:19 +0530

    q7

--- rest/index.js
+++ rest/index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const conn = require("./db");
 
-conn.on("connected ", () => {
- console.log("Connected to MongoDB! ");
+conn.on("connected", () => {
+    console.log("Connected to MongoDB!");
 });
 
 app.use(express.json());
 app.use(cors());
 
-
-
 //routes
-app.use( "/users", require("./routes/users")) ;
+app.use("/users", require("./routes/users"));
 
 app.listen(PORT, () => console.log("Server Is Running On Port: " + PORT));
