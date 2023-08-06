c4b11729bd15b705d475daab1cba4210d2846fa0
jaydeepbendre <jaydeepd.bendre@gmail.com>
2023-08-06 18:38:28 +0530

    q9f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the build
+    outDir: 'dist',
   },
-})
+  define: {
+    // Define your base URL here
+    BASE_URL: JSON.stringify('https://uesrapp.onrender.com'),
+  },
+});
